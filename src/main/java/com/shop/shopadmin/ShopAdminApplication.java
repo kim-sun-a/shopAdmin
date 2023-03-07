b9d5147cb9e51e1ff7e91679932b1f8661204dc7
sunakim <sakim@icomas.co.kr>
2023-03-07 11:16:43 +0900

    db 설정 오류 변경

--- src/main/java/com/shop/shopadmin/ShopAdminApplication.java
+++ src/main/java/com/shop/shopadmin/ShopAdminApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopAdminApplication {
 
     public static void main(String[] args) {
+
+        String profile = System.getProperty("spring.profiles.active");
+        if(profile == null) {
+            System.setProperty("spring.profiles.active", "local");
+        }
         SpringApplication.run(ShopAdminApplication.class, args);
     }
 
